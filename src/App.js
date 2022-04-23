472f6b7d3338c19d9978a192939a2ce96fca3524
Peter <phellwege1@gmail.com>
2022-04-23 14:29:18 -0600

    view bkg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import Home from './views/Home';
+import Crew from './views/Crew';
+import Tech from './views/Technology';
+import Dest from './views/Destination';
 
 import './App.css';
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <React.Fragment>
           <Router>
             <Home path="/"/>
+            <Crew path='/crew'/>
+            <Tech path='/tech'/>
+            <Dest path='/destination'/>
           </Router>
       </React.Fragment>
     </div>
--- src/views/Crew.js
+++ src/views/Crew.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import '../App.css';
 
 
 export default () => {
     return (
-        <div id="background">
+        <div id="CrewBkg">
             
         </div>
     )
