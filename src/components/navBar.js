31dae2a99afc9e8dd8d71eb2690cdd21920d12d9
Peter <phellwege1@gmail.com>
2022-04-23 15:27:57 -0600

    started nav

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,12 +5,16 @@ import '../App.css';
 export default () => {
     return (
         <div id="navBarWrap">
+            <div></div>
+            <div><p>-------------------------------------</p></div>
+            <div id='navBarLinks'>
             <ul>
-                <li><a href='/' id='navText'>00 Home</a></li>
-                <li><a href='/' id='navText'>01 Destination</a></li>
-                <li><a href='/' id='navText'>02 Crew</a></li>
-                <li><a href='/' id='navText'>03 Technology</a></li>
+                <li><a href='#/' id='navText'>00 Home</a></li>
+                <li><a href='#/destination' id='navText'>01 Destination</a></li>
+                <li><a href='#/crew' id='navText'>02 Crew</a></li>
+                <li><a href='#/tech' id='navText'>03 Technology</a></li>
             </ul>
+            </div>
         </div>
     )
 }
