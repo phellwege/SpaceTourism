8626529fa13d5daaa53a466c0fccb9e08740781d
Peter <phellwege1@gmail.com>
2022-04-23 15:32:37 -0600

    working through basic homepage styling

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -9,10 +9,10 @@ export default () => {
             <div><p>-------------------------------------</p></div>
             <div id='navBarLinks'>
             <ul>
-                <li><a href='#/' id='navText'>00 Home</a></li>
-                <li><a href='#/destination' id='navText'>01 Destination</a></li>
-                <li><a href='#/crew' id='navText'>02 Crew</a></li>
-                <li><a href='#/tech' id='navText'>03 Technology</a></li>
+                <li><a href='/' id='navText'>00 Home</a></li>
+                <li><a href='/destination' id='navText'>01 Destination</a></li>
+                <li><a href='/crew' id='navText'>02 Crew</a></li>
+                <li><a href='/tech' id='navText'>03 Technology</a></li>
             </ul>
             </div>
         </div>
--- src/views/Technology.js
+++ src/views/Technology.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import '../App.css';
-
+import NavBar from '../components/navBar';
 
 export default () => {
     return (
         <div id="TechBkg">
-            
+            <NavBar/>
         </div>
     )
 }
