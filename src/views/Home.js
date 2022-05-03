1aa9116eb31e81a2649cb0925b2b520dbe51ad1b
phellweg <peter.hellwege@vizientinc.com>
2022-05-03 13:51:48 -0600

    scaling correction

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,17 +8,17 @@ export default () => {
             <NavBar/>
             <div id='HomeWrap'>
                 <div>
-                    <h5>So, you want to travel to</h5>
-                    <h2>Space</h2>
-                    <p>Let’s face it; if you want to go to space, you might as well genuinely go to <br/> 
-                    outer space and not hover kind of on the edge of it. Well sit back, and relax <br/>
-                    because we’ll give you a truly out of this world experience!
+                    <h4>So, you want to travel to</h4>
+                    <h2>SPACE</h2>
+                    <p>Let’s face it; if you want to go to space, you might as well <br/>genuinely go to 
+                    outer space and not hover kind of on the <br/>edge of it. Well sit back, and relax 
+                    because we’ll give you<br/> a truly out of this world experience!
                     </p>
                 </div>
                 <div>
                     <div id='ExploreCircle'>
                         <a href='/'>
-                            <h4>Explore</h4>
+                            <h4>EXPLORE</h4>
                         </a>
                     </div>
                 </div>
