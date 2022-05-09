21175701a5b49a2875bbe822d0f3d82af9502474
phellweg <peter.hellwege@vizientinc.com>
2022-05-09 14:57:04 -0600

    starting to cook this hamburger

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,8 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import '../App.css';
-import logo from '../static/shared/logo.svg'
-import Hamburger from '../static/shared/icon-hamburger.svg'
+import logo from '../static/shared/logo.svg';
+import Hamburger from '../static/shared/icon-hamburger.svg';
+import close from '../static/shared/icon-close.svg';
+
 export default (props) => {
+        const [navbarOpen, setNavbarOpen] = useState(false)
+        const handleToggle = () => {
+            setNavbarOpen(prev => !prev)
+        }
+        const closeMenu = () => {
+            setNavbarOpen(false)
+        }
     const {active}=props;
     return (
         <div id="navBarWrap" className="sticky">
@@ -16,9 +25,22 @@ export default (props) => {
                 <li><a href='/tech' id='navText'  className={"links"+(active===3?" active":"")}><span id='darkText'>03</span> Technology</a></li>
             </ul>
             </div>
-            <div id='hamMenu'>
-                <img src={Hamburger}/>
-            </div>
+            <div className='Ham'>
+                <nav className="navBar">
+                        <button onClick={handleToggle}>{navbarOpen ? (
+                        <img src = {close} />
+                        ) : (
+                        <img src={Hamburger} />
+                        )}
+                        </button>
+                            <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+                                <li><a class="active" href="/" onClick={() => closeMenu()}><span id='darkText'>00</span> Home</a></li>
+                                <li><a href="/destination"  onClick={() => closeMenu()}><span id='darkText'>01</span> Destination</a></li>
+                                <li><a href="/crew" onClick={() => closeMenu()}><span id='darkText'>02</span> Crew</a></li>
+                                <a href='/tech' onClick={() => closeMenu()}><span id='darkText'>03</span> Technology</a>
+                            </ul>
+                    </nav>
+                </div>
         </div>
     )
 }
